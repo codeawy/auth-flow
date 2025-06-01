2510d2c351313886cd510892b775faeee8f01adf
codeawy <codeawy@gmail.com>
2025-06-02 02:07:43 +0300

    Basic auth resource and API prefix

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,34 +4,33 @@ import { faker } from '@faker-js/faker';
 const prisma = new PrismaClient();
 
 export default async function main() {
-  await prisma.user.createMany({
-    data: Array.from({ length: 25 }, () => ({
-      firstName: faker.person.firstName(),
-      lastName: faker.person.lastName(),
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-      isEmailVerified: faker.datatype.boolean(),
-      provider: faker.helpers.arrayElement([
-        'EMAIL_PASSWORD',
-        'GOOGLE',
-        'GITHUB',
-      ]),
-    })),
-  });
-
-  await prisma.todo.createMany({
-    data: Array.from({ length: 100 }, () => ({
-      title: faker.lorem.sentence(),
-      description: faker.lorem.paragraph(),
-      completed: faker.datatype.boolean(),
-      priority: faker.helpers.arrayElement(['LOW', 'MEDIUM', 'HIGH']),
-      userId: faker.helpers.arrayElement([
-        // TODO: after migration you should get users ids from db
-        '394c54ff-07a3-48c2-afea-c4b12f9aea41',
-        '46afd3d3-b189-4d6e-a122-0633b472aa3b',
-      ]),
-    })),
-  });
+  // await prisma.user.createMany({
+  //   data: Array.from({ length: 25 }, () => ({
+  //     firstName: faker.person.firstName(),
+  //     lastName: faker.person.lastName(),
+  //     email: faker.internet.email(),
+  //     password: faker.internet.password(),
+  //     isEmailVerified: faker.datatype.boolean(),
+  //     provider: faker.helpers.arrayElement([
+  //       'EMAIL_PASSWORD',
+  //       'GOOGLE',
+  //       'GITHUB',
+  //     ]),
+  //   })),
+  // });
+  // await prisma.todo.createMany({
+  //   data: Array.from({ length: 10 }, () => ({
+  //     title: faker.lorem.sentence(),
+  //     description: faker.lorem.paragraph(),
+  //     completed: faker.datatype.boolean(),
+  //     priority: faker.helpers.arrayElement(['LOW', 'MEDIUM', 'HIGH']),
+  //     userId: faker.helpers.arrayElement([
+  //       // TODO: after migration you should get users ids from db
+  //       'f0ee13d8-3ab0-4a4f-9119-72db8d8290eb',
+  //       '082f57a7-895b-457d-aa5e-df093d00ee43',
+  //     ]),
+  //   })),
+  // });
 }
 
 main()
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Set global prefix for all routes
+  const apiPrefix = 'api';
+  app.setGlobalPrefix(apiPrefix);
+
   await app.listen(process.env.PORT ?? 5001);
 }
-bootstrap();
+bootstrap()
+  .then(() => {
+    console.log('Server started on port', process.env.PORT ?? 5001);
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
