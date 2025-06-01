ff67679cdc798064fec50540e1aa363f16a44e70
codeawy <codeawy@gmail.com>
2025-06-01 22:39:48 +0300

    Get user todo's

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,21 +1,36 @@
 import { PrismaClient } from './generated/prisma';
+import { faker } from '@faker-js/faker';
 
 const prisma = new PrismaClient();
 
 export default async function main() {
   await prisma.user.createMany({
-    data: [
-      {
-        name: 'John Doe',
-        email: 'john.doe@example.com',
-        password: 'password',
-      },
-      {
-        name: 'Jane Doe',
-        email: 'jane.doe@example.com',
-        password: 'password',
-      },
-    ],
+    data: Array.from({ length: 25 }, () => ({
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      isEmailVerified: faker.datatype.boolean(),
+      provider: faker.helpers.arrayElement([
+        'EMAIL_PASSWORD',
+        'GOOGLE',
+        'GITHUB',
+      ]),
+    })),
+  });
+
+  await prisma.todo.createMany({
+    data: Array.from({ length: 100 }, () => ({
+      title: faker.lorem.sentence(),
+      description: faker.lorem.paragraph(),
+      completed: faker.datatype.boolean(),
+      priority: faker.helpers.arrayElement(['LOW', 'MEDIUM', 'HIGH']),
+      userId: faker.helpers.arrayElement([
+        // TODO: after migration you should get users ids from db
+        '394c54ff-07a3-48c2-afea-c4b12f9aea41',
+        '46afd3d3-b189-4d6e-a122-0633b472aa3b',
+      ]),
+    })),
   });
 }
 
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from 'prisma/generated/prisma';
-import { User } from 'prisma/generated/prisma';
 
 @Injectable()
 export class AppService {
   constructor(private readonly prisma: PrismaClient) {}
 
-  getUserList(): Promise<User[]> {
-    return this.prisma.user.findMany();
+  getUserList() {
+    return this.prisma.user.findMany({
+      select: {
+        id: true,
+        firstName: true,
+        lastName: true,
+        email: true,
+        isEmailVerified: true,
+        provider: true,
+        todos: {
+          select: {
+            id: true,
+            title: true,
+            completed: true,
+            priority: true,
+            userId: true,
+            user: {
+              select: {
+                id: true,
+                email: true,
+              },
+            },
+          },
+        },
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
   }
 }
