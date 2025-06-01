601c353d048651df7a38fc0704e97f0045a92a41
codeawy <codeawy@gmail.com>
2025-06-01 22:41:04 +0300

    Change controller returns

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { User } from 'prisma/generated/prisma';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getUserList(): Promise<User[]> {
+  getUserList() {
     return this.appService.getUserList();
   }
 }
