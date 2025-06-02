63faa09d9b2625e551482359dc7308296f617e63
codeawy <codeawy@gmail.com>
2025-06-02 03:06:49 +0300

    Validate user registeration data

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,6 +8,14 @@ async function bootstrap() {
   // Set global prefix for all routes
   const apiPrefix = 'api';
   app.setGlobalPrefix(apiPrefix);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 5001);
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,32 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail({}, { message: 'Please enter a valid email address' })
+  email: string;
+
+  @MinLength(7, { message: 'Password must be at least 7 characters long' })
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{}:;"'|,.<>/?])(?=.{7,})/,
+    {
+      message:
+        'Password must include lowercase, uppercase, numbers, and symbols',
+    },
+  )
+  password?: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(2, { message: 'First name must be at least 2 characters long' })
+  firstName?: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(2, { message: 'Last name must be at least 2 characters long' })
+  lastName?: string;
+}
